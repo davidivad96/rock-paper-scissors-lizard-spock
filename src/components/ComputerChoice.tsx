2da4a06314d8735e6b66bfb9c956923058048883
davidivad96 <david@strivelabs.io>
2022-02-11 14:01:36 +0100

    Implement ComputerChoice component

--- src/components/ComputerChoice.tsx
+++ src/components/ComputerChoice.tsx
@@ -1,5 +1,31 @@
+import { Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { GameChoice } from '../interfaces';
 import { OptionButton } from './GameButtons';
+import paper from '../../public/icon-paper.svg';
+import scissors from '../../public/icon-scissors.svg';
+import rock from '../../public/icon-rock.svg';
+
+const images = {
+  paper,
+  scissors,
+  rock,
+};
+
+const styles = {
+  paper: {
+    border: '15px solid hsl(230, 89%, 65%)',
+    boxShadow: 'inset 0px 6px rgb(0 0 0 / 20%), 0 6px hsl(230, 89%, 56%)',
+  },
+  scissors: {
+    border: '15px solid hsl(40, 84%, 53%)',
+    boxShadow: 'inset 0px 6px rgb(0 0 0 / 20%), 0 6px hsl(39, 89%, 43%)',
+  },
+  rock: {
+    border: '15px solid hsl(349, 70%, 56%)',
+    boxShadow: 'inset 0px 6px rgb(0 0 0 / 20%), 0 6px hsl(349, 71%, 46%)',
+  },
+};
 
 interface Props {
   isSmallDevice: boolean;
@@ -7,6 +33,25 @@ interface Props {
 }
 
 const ComputerChoice: React.FunctionComponent<Props> = ({ isSmallDevice, gameChoice }) => {
+  const [time, setTime] = useState<number>(3);
+  const [computerChoice, setComputerChoice] = useState<GameChoice>('');
+
+  useEffect(() => {
+    if (gameChoice !== '') {
+      setTime(3);
+      const timer = setInterval(() => {
+        setTime((time) => {
+          if (time === 1) {
+            const choices: GameChoice[] = ['rock', 'paper', 'scissors'];
+            setComputerChoice(choices[Math.floor(Math.random() * choices.length)]);
+          }
+          return time - 1;
+        });
+      }, 700);
+      return () => clearInterval(timer);
+    }
+  }, [gameChoice]);
+
   return (
     <OptionButton
       sx={{
@@ -15,11 +60,21 @@ const ComputerChoice: React.FunctionComponent<Props> = ({ isSmallDevice, gameCho
         visibility: gameChoice === '' ? 'hidden' : 'visible',
         transform: `translate3d(${isSmallDevice ? '110px' : '170px'}, -50px, 0) scale(1.3)`,
         cursor: 'default',
-        border: 'none',
-        backgroundColor: 'rgba(0,0,0,0.2)',
+        border: `${computerChoice !== '' ? styles[computerChoice].border : 'none'}`,
+        boxShadow: `${computerChoice !== '' ? styles[computerChoice].boxShadow : 'none'}`,
+        backgroundColor: `${computerChoice !== '' ? '#FFF' : 'rgba(0, 0, 0, 0.2)'}`,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      hello
+      {computerChoice === '' ? (
+        <Typography variant="h3" fontWeight="bold">
+          {time}
+        </Typography>
+      ) : (
+        <img src={images[computerChoice]} alt="computer choice" width={isSmallDevice ? '45px' : '65px'} />
+      )}
     </OptionButton>
   );
 };
